3e90f2c33914129286f359eef742813607299351
ltellez2018 <ltellez.2009@gmail.com>
2021-04-09 10:31:17 -0500

    First Commit

---
+++ src/Helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=GpXW1nZoV6t5u8gdNdGsTzphjYt4M0mf`;
+    const resp  = await fetch(url);
+    const  { data } =  await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+};
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,11 @@
+export const GifGridItem = ( {title, url})  => {
+    console.log(title, url);
+
+    return (
+        <div className="card animate__animated animate__fadeIn">
+          <img src={url} alt={title}></img>
+          <p>{title}</p>
+        </div>
+    )
+
+}
